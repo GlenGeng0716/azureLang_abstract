c733641d81faa6450fe3dd63d74ee047e61512bb
GlenGeng0716 <glengeng0716@gmail.com>
2020-03-05 15:13:15 +0100

    RBAC development on resource group scope level and related test cases.

--- src/test/java/com/foreseeti/azureLang/test/TestRBAC.java
+++ src/test/java/com/foreseeti/azureLang/test/TestRBAC.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 
 public class TestRBAC extends azureLangTest {
     private static class ReadSubscriptionModel {
-        // A simple azure account login model
+        // A simple RBAC model, reader role affect on subscription
         
         public final Reader reader = new Reader("reader");
         public final ADUser aduser = new ADUser("aduser");
@@ -50,8 +50,82 @@ public class TestRBAC extends azureLangTest {
         model.subscription.write.assertUncompromised();
     }
     
-   
-    
+    private static class ReadSubscriptionModelwithAction{
+        // A simple RBAC model, reader role affect on subscription using action 'SubscriptionRead'
+        
+        public final Reader reader = new Reader("reader");
+        public final ADUser aduser = new ADUser("aduser");
+        public final Subscription subscription = new Subscription("subscription");
+        public final SubscriptionRead subscriptionread = new SubscriptionRead("subscriptionread");
+        public final ResourceGroup resourcegroup = new ResourceGroup("resourcegroup");
+        
+        public ReadSubscriptionModelwithAction(){
+            aduser.addPrincipalReader(reader);
+            reader.addExecutees(subscriptionread);
+            subscriptionread.addScopes(subscription);
+            subscription.addResourcegroup(resourcegroup);
+        }
     }
+    
+    @Test
+    @DisplayName("Test Reader role of RBAC affect on the subscription using the action 'SubscriptionRead'")
+    public void testReaderRBAConSubscriptionwithAction() {
+        var model = new ReadSubscriptionModelwithAction();
+        
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(model.aduser.assume);
+        attacker.attack();
+        
+        model.subscription.read.assertCompromisedInstantaneously();
+        model.subscription.write.assertUncompromised();
+    }
+    
+    @Test
+    @DisplayName("Test the Reader Role can affect on the resource group scope from a higher scope, subscription.")
+    public void testReaderRBAConResourceGroupfromSubscription() {
+        var model = new ReadSubscriptionModelwithAction();
+        
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(model.aduser.assume);
+        attacker.attack();
+        
+        model.resourcegroup.read.assertCompromisedInstantaneously();
+        model.resourcegroup.write.assertUncompromised();
+        
+        
+    }
+    
+    private static class ReadResourceGroupwithAction {
+        public final Reader reader = new Reader("reader");
+        public final ADUser aduser = new ADUser("aduser");
+        public final ResourceGroup resourcegroup = new ResourceGroup("resourcegroup");
+        
+        
+        public final ResourceGroupRead resourcegroupread = new ResourceGroupRead("resourcegroupread");
+        public final AzureServiceResource azureserviceresource = new AzureServiceResource("azureserviceresource");
+        
+        public ReadResourceGroupwithAction() {
+            aduser.addPrincipalReader(reader);
+            reader.addExecutees(resourcegroupread);
+            resourcegroupread.addScopes(resourcegroup);
+            resourcegroup.addAzureResources(azureserviceresource);
+        }
+    }
+    
+    @Test
+    @DisplayName("Test the Read Role of RBAC affect on the Resource Group scope. In other words, the AD user has been distributed a reader role of a resource group.")
+    public void testReaderRBAConResourceGroupwithAction() {
+        var model = new ReadResourceGroupwithAction();
+        
+        Attacker attacker = new Attacker();
+        attacker.addAttackPoint(model.aduser.assume);
+        attacker.attack();
+        
+        model.resourcegroup.read.assertCompromisedInstantaneously();
+        model.resourcegroup.write.assertUncompromised();
+        model.azureserviceresource.read.assertCompromisedInstantaneously();
+        model.azureserviceresource.write.assertUncompromised();
+    }
+}
 
 
